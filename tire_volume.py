b1202c7bdd370b2c76902e587f4f9a8addb4b57b
pedro-caldas-github <josepedro.caldas@gmail.com>
2025-11-07 07:18:57 -0300

    first asigment

---
+++ tire_volume.py
@@ -0,0 +1,10 @@
+import math
+v = 0
+
+w = int(input("type the width of the tire(ex:205): "))
+a = int(input("type the aspect ratio of the tire(ex:60): "))
+d = int(input("type the diameter of the tire(ex:16): "))
+
+v = math.pi*w**2*a*(w*a+2540*d)/10000000000
+
+print(f"your tire volume is:{v:.2f}")
